9ad9f7eca7422e2770c17f2785c93c3f4b4d46a9
Pedro Paes Siniscalchi <ppaes94_ps@hotmail.com>
2020-01-27 18:16:56 -0300

    Python_OO

--- Python_OO.py
+++ Python_OO.py
@@ -1,6 +1,13 @@
-numero = 123
-titular = "Pedro"
-saldo = 55.00
-limita = 1000.0
 
-conta = {"numero": 123, "titular": "Pedro", "saldo": 55.0, "limite": 1000.0}
+def cria_conta(numero, titular, saldo, limite):
+    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
+    return conta
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+def saca(conta, valor):
+        conta["saldo"] -= valor
+
+def extrato(conta):
+    print("O saldo da conta é {} ".format(conta["saldo"]))
